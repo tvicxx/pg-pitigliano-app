227aab749b23934aefce92f515262a67db8f9dd6
Tommaso <104839987+tvicxx@users.noreply.github.com>
2026-08-04 12:23:42 +0200

    inseriti altri testi dei canti

--- canti.js
+++ canti.js
@@ -9,7 +9,7 @@ const canti = [
   {
     titolo: "Frutto della nostra terra",
     categoria: "Offertorio / Comunione",
-    testo: "Frutto della nostra terra, del lavoro di ogni uomo\npane della nostra vita, cibo della quotidianità.\nTu che lo prendevi un giorno,\nlo spezzavi per i tuoi,\noggi vieni in questo pane e ti doni per la vita mia.\n\n<b>RIT: E sarò pane, e sarò vino\nNella mia vita, nelle tue mani.\nTi accoglierò dentro di me\nFarò di me un’offerta viva,\nun sacrificio gradito a te.</b>\n\nFrutto della nostra terra, del lavoro di ogni uomo\nvino delle nostre vigne sulla mensa dei fratelli tuoi.\nTu che lo prendevi un giorno,\nlo bevevi con i tuoi.\nOggi vieni in questo vino\ne ti doni per la vita mia <b> RIT.</b>"
+    testo: "Frutto della nostra terra, del lavoro di ogni uomo\npane della nostra vita, cibo della quotidianità.\nTu che lo prendevi un giorno,\nlo spezzavi per i tuoi,\noggi vieni in questo pane e ti doni per la vita mia.\n\n<b>RIT: E sarò pane, e sarò vino\nNella mia vita, nelle tue mani.\nTi accoglierò dentro di me\nFarò di me un’offerta viva,\nun sacrificio gradito a te.</b>\n\nFrutto della nostra terra, del lavoro di ogni uomo\nvino delle nostre vigne sulla mensa dei fratelli tuoi.\nTu che lo prendevi un giorno,\nlo bevevi con i tuoi.\nOggi vieni in questo vino e ti doni per la vita mia <b> RIT.</b>"
   },
   {
     titolo: "Il canto dell'amore",
@@ -19,7 +19,7 @@ const canti = [
   {
     titolo: "Re dei re",
     categoria: "Ingresso / Finale",
-    testo: "Hai sollevato i nostri volti dalla polvere\nLe nostre colpe hai portato su di te\nSignore ti sei fatto uomo in tutto come noi,\nPer a-mo-re\n<b>RIT: Figlio dell'Altissimo, povero tra i poveri\nVieni a dimorare tra noi. Dio dell'impossibile,\nRe di tutti i secoli Vieni nella tua maestà\nRe dei re, i popoli ti acclamano\nI cieli ti proclamano Re dei re\nLuce degli uomini, regna col tuo amore\ntra noi, noi, noi, noi</b>\n\nCi hai riscattati dalla stretta delle tenebre\nPerché potessimo glorificare te\nHai riversato in noi la vita del tuo Spirito,\nPer a-mo-re <b>RIT.</b>\n\nTua è la glo – ria per se – mpre\nTua è la glo – ria per se – mpre\nGlo – ria, glo – ria Glo – ria, glo – ria <b>RIT.</b>"
+    testo: "Hai sollevato i nostri volti dalla polvere\nLe nostre colpe hai portato su di te\nSignore ti sei fatto uomo in tutto come noi,\nPer a-mo-re\n\n<b>RIT: Figlio dell'Altissimo, povero tra i poveri\nVieni a dimorare tra noi. Dio dell'impossibile,\nRe di tutti i secoli Vieni nella tua maestà\nRe dei re, i popoli ti acclamano\nI cieli ti proclamano Re dei re\nLuce degli uomini, regna col tuo amore\ntra noi, noi, noi, noi</b>\n\nCi hai riscattati dalla stretta delle tenebre\nPerché potessimo glorificare te\nHai riversato in noi la vita del tuo Spirito,\nPer a-mo-re <b>RIT.</b>\n\nTua è la glo – ria per se – mpre\nTua è la glo – ria per se – mpre\nGlo – ria, glo – ria Glo – ria, glo – ria <b>RIT.</b>"
   },
   {
     titolo: "Soffierà",
@@ -34,6 +34,71 @@ const canti = [
   {
     titolo: "Servo per amore",
     categoria: "Ingresso / Finale",
-    testo: "Una notte di sudore, sulla barca in mezzo al mare\ne mentre il cielo s’imbianca già,\ntu guardi le tue reti vuote.\nMa la voce che ti chiama, un altro mare ti mostrerà\ne sulle rive di ogni cuore, le tue reti getterai.\n\n<b>RIT: Offri la vita tua come Maria\nai piedi della croce e sarai,\nservo di ogni uomo, servo per amore\nsacerdote dell’umanità.</b>\n\nAvanzavi nel silenzio, fra le lacrime e speravi\nche il seme sparso davanti a te, cadesse sulla buonaterra. \nOra il cuore tuo è in festa, perché il grano biondeggia ormai,\n è maturato sotto il sole, puoi riporlo nei granai. <b>RIT.</b>"
+    testo: "Una notte di sudore, sulla barca in mezzo al mare\ne mentre il cielo s’imbianca già,tu guardi le tue reti vuote.\nMa la voce che ti chiama, un altro mare ti mostrerà\ne sulle rive di ogni cuore, le tue reti getterai.\n\n<b>RIT: Offri la vita tua come Maria\nai piedi della croce e sarai,\nservo di ogni uomo, servo per amore\nsacerdote dell’umanità.</b>\n\nAvanzavi nel silenzio, fra le lacrime e speravi\nche il seme sparso davanti a te, cadesse sulla buonaterra. \nOra il cuore tuo è in festa, perché il grano biondeggia ormai,\nè maturato sotto il sole, puoi riporlo nei granai. <b>RIT.</b>"
+  },
+  {
+    titolo: "Insieme è più bello",
+    categoria: "Ingresso / Finale",
+    testo: "Dietro i volti sconosciuti della gente che mi sfiora\nquanta vita, quante attese di felicità\nQuanti attimi vissuti,\nmondi da scoprire ancora\nsplendidi universi accanto a me.\n\n<b>RIT: È più bello insieme\nè un dono grande l’altra gente! È più bello insieme.\nÈ più bello insieme,\nè un dono grande l’altra gente!È più bello insieme</b>\n\nE raccolgo nel mio cuore la speranza ed il dolore,\nil silenzio e il canto della gente come me.\nIn quel pianto, in quel sorriso,\nè il mio pianto, il mio sorriso\nchi mi vive accanto è un altro me. <b>RIT.</b>\n\nFra le case, i grattacieli, fra le antenne lassù in alto\ncosì trasparente il cielo non l’ho visto mai.\nE la luce getta veli\ndi colore sull’asfalto\npuoi anche cantarli assieme a me. <b>RIT.</b>"
+  },
+  {
+    titolo: "Grazie",
+    categoria: "Ingresso / Finale",
+    testo: "<b>RIT. Grazie, grazie, grazie perché\ntu sei vicino a me (2V).</b>\n\nOgni giorno io ti cerco, ogni giorno io ti trovo,\nogni giorno ti ringrazio Dio di averti qui vicino.\nTu fai parte di me stesso, sei la gioia sei l’amore,\ntu discendi nel miocuore e lo porti via con te. <b>RIT.</b>\n\nLa mia vita ora ha un senso,la mia vita ha un valore,\ntutto questo solamente per avermi aperto il cuore.\nTi ringrazio mio Signore, ti ringrazio totalmente,\nper avermi amato sempre e portato via con te. <b>RIT.</b>\n\nQuante volte ho pensato di lasciarti andare via,\ndi lasciare questo amore che mi lega e mi trascina,\nma nel cuore c’è sempre nostalgia delle parole,\nche tu dici al mio cuore quando io sono con te. <b>RIT.</b>"
+  },
+  {
+    titolo: "Dio",
+    categoria: "Ingresso / Finale",
+    testo: "Esisti da sempre in tutta la tua grandezza\nEd io ti scopro nella luce del cielo,\nnella bellezza dei monti\nnella vastità dei mari,\nnei fiori di campo\nnel sorriso di un bimbo,\nche dorme con te.\n\n<b>RIT. Dio, sei sempre con noi,\nin ogni ora del giorno, sei forza,\nsei vita,sei gioia infinita. (2v)</b>\n\nTi cerco mio Dio nelle giornate indifferenti\nPer colmare quel gran vuoto che mi assale,\ne ti desidero accanto a me,\nper darmi la tua forza,\nper darmi il tuo coraggio,\nso che sei quella luce,\nche veglia in me. <b>RIT.</b>"
+  },
+  {
+    titolo: "Un amico",
+    categoria: "Offertorio / Comunione",
+    testo: "Ho trovato finalmente nel cielo\nUn tesoro un amico vero\nL’ orizzonte adesso è ancora più chiaro,\nE ho dipinto una stella nel cielo\n\n<b>RIT. Un amico la speranza un amico verità\nChe ti porga l’altra guancia un amico lui sarà\nUn amico che tu sappia che non ti tradirà\nUn amico mio sincero, un amico vero</b>\n\nIl tramonto che ci spegne la sera\nE la notte è piùscura e più nera.\nIo ti voglio sempre più vicino,\nQuando l’alba ci sveglia al mattino. <b>RIT.</b>\n\nIl mio sogno finalmente si è avverato,\nCiò che io cercavo l’ho trovato.\nLui mi guiderà per tutto il cammino.\nE mi stringerà più forte la mano. <b>RIT.</b>\n\n<b>L’amicizia questo è tutto ciò che ci basterà\nUn amico chee non dica mai no\nSe la vita ci riserva tante cose però\nUn amico vero io ce l’ho Amico mio\nUn amico vero io ce l’ho solo Tu Dio\nUn amico vero Tu mio Dio</b>"
+  },
+  {
+    titolo: "Popoli tutti acclamate",
+    categoria: "Offertorio / Comunione",
+    testo: "Mio Dio, Signore, nulla è pari a te\nOra e per sempre, voglio lodare\nil tuo grande amor per me\nMia roccia tu sei, pace e conforto mi dai\ncon tutto il cuore e le mie forze\nSempre io ti adorerò\n\n<b>RIT: Popoli tutti acclamate al Signore\nGloria e potenza cantiamo al re\nMari e Monti si prostrino a te\nAl tuo nome, o Signore\nCanto di gioia per quello che fai\nPer sempre Signore con te resterò\nNon c'è promessa, non c'è fedeltà che in te</b>\n\nMio Dio, Signore, nulla è pari a te\nOra e per sempre, voglio lodare\nIl tuo grande amor per me\nMia roccia tu sei, pace e conforto mi dai\nCon tutto il cuore e le mie forze\nSempre io ti adorerò\n\n<b>RIT: Popoli tutti acclamate al Signore\nGloria e potenza cantiamo al re\nMari e Monti si prostrino a te\nAl tuo nome, o Signore\nCanto di gioia per quello che fai\nPer sempre Signore con te resterò\nNon c'è promessa, non c'è fedeltà che in te\nNon c'è promessa, non c'è fedeltà che in te</b>"
+  },
+  {
+    titolo: "Come tu mi vuoi",
+    categoria: "Offertorio / Comunione",
+    testo: "Eccomi Signor, vengo a Te mio Re\nche si compia in me la Tua volontà\nEccomi signore, vengo a Te mio Dio,\nplasma il cuore mio e di Te vivrò\nSe Tu lo vuoi Signore manda me\ne il Tuo nome annuncerò\n\n<b>RIT: Come Tu mi vuoi io sarò,\ndove Tu mi vuoi io andrò.\nQuesta vita io voglio donarla a Te\nper dare gloria al Tuo nome mio re.\nCome Tu mi vuoi io sarò,\ndove Tu mi vuoi io andrò,\nse mi guida il Tuo amore paura non ho\nper sempre io sarò, come Tu mi vuoi.</b>\n\nEccomi Signor, vengo a Te mio Re\nche si compia in me la Tua volontà\nEccomi signore, vengo a Te mio Dio,\nplasma il cuore mio e di Te vivrò\nTra le Tue mani mai più vacillerò\ne strumento Tuo sarò <b>RIT.</b>"
+  },
+  {
+    titolo: "Vocazione",
+    categoria: "Offertorio / Comunione",
+    testo: "Era un giorno come tanti altri, e quel giorno lui passò\nEra un uomo come tanti altri, e passando mi chiamò\nCome lo sapesse che il mio nome era proprio quello\nCome mai volesse proprio me nella sua vita non lo so\nEra un giorno come tanti altri e quel giorno mi chiamò\n\n<b>RIT: Tu Dio che conosci il nome mio\nfa che ascoltando la tua voce\nio ricordi dove porta la mia strada\nnella vita all'incontro con Te.</b>\n\nEra un'alba triste e senza vita, e qualcuno mi chiamò\nEra un uomo come tanti altri, ma la voce quella no\nQuante volte un uomo con il nome giusto mi ha chiamato\nuna volta sola l'ho sentito pronunciare con amore.\nEra un uomo come nessun altro e passando mi chiamò <b>RIT.</b>"
+  },
+  {
+    titolo: "Emmanuel",
+    categoria: "Ingresso / Finale",
+    testo: ""
+  },
+  {
+    titolo: "Jubilate Deo",
+    categoria: "Ingresso / Finale",
+    testo: "Jubilate Deo, omins terra\nServite Domino in laetitia\nAlleluia, alleluia, in laetitia\nAlleluia, alleluia, in laetitia\n"
+  },
+  {
+    titolo: "Il disegno",
+    categoria: "Offertorio / Comunione",
+    testo: "Nel mare del silenzio\nuna voce si alzò\nda una notte senza confini\nuna luce brillò\ndove non c'era niente quel giorno.\n\n<b>RIT: Avevi scritto già\nil mio nome lassù nel cielo,\navevi scritto già\nla mia vita insieme a te\navevi scritto già di me</b>\n\nE quando la tua mente\nfece splendere le stelle\ne quando le tue mani\modellarono la terra\ndove non c'era niente quel giorno <b>RIT.</b>\n\nE quando hai calcolato\nle profondità del cielo\ne quando hai calcolato\nogni fiore della terra\dove non c'era niente quel giorno <b>RIT.</b>\n\nE quando hai disegnato\nle nubi e le montagne\ne quando hai disegnato\nil cammino di ogni uomo\nl'avevi fatto anche per me.\n\n<b>Se ieri non sapevo\noggi ho incontrato Te\ne la mia libertà\nè il tuo disegno su di me\nnon cercherò più niente perché...\nTu mi salverai</b>"
+  },
+  {
+    titolo: "Se m'accogli",
+    categoria: "Ingresso / Finale",
+    testo: "Tra le mani non ho niente\nspero che mi accoglierai\nspero solo di restare accanto a te.\nSono ricco solamente \ndell'amore che mi dai:\ne per quelli che\nnon l'hanno avuto mai\n\n<b>RIT: Se m'accogli, mio Signore\naltro non ti chiederò\ne per sempre la tua strada\nla mia strada resterà\nnella gioia e nel dolore\nfino a quando tu vorrai\ncon la mano nella Tua\ncamminerò.</b>\n\nIo ti prego con il cuore,\nso che tu mi ascolterai\nrendi forte la mia fede più che mai.\nTieni accesa la mia luce\nfino al giorno che tu sai,\ncon i miei fratelli\nin contro a te verrò. <b>RIT.</b>"
+  },
+  {
+    titolo: "Dall'aurora al tramonto",
+    categoria: "Ingresso / Finale",
+    testo: "<b>RIT: Dall'aurora io cerco te\nfino al tramonto ti chiamo,\nha sete solo di te\nl'anima mia come terra deserta</b>\n\nNon mi fermerò un solo istante,\nsempre canterò la tua lode\nperché sei il mio Dio il mio riparo,\nmi proteggerai all'ombra delle tue ali <b>RIT.</b>\n\nNon mi fermerò un solo istante\nio racconterò le tue opere\nperché sei il mio Dio, unico bene,\nnulla mai potrà la notte contro di me. <b>RIT.</b>"
+  },
+  {
+    titolo: "Resta qui con noi",
+    categoria: "Ingresso / Finale",
+    testo: "Le ombre si distendono\nscende ormai la sera\ne s'allontanano dietro i monti\ni riflessi di un giorno che non finirà\ndi un giorno che ora correrà sempre\nperché sappiamo che una nuova vita\nda qui è partita e mai più si fermerà\n\n<b>RIT: Resta qui con noi il sole scende già\nresta qui con noi Signore è sera ormai\nResta qui con noi il sole scende già\nse Tu sei fra noi la notte non verrà</b>\n\n"
   }
 ];
