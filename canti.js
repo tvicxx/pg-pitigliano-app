4401c085209d379fa974a3e2b30ff1c82c5491e1
Tommaso <104839987+tvicxx@users.noreply.github.com>
2026-08-04 09:46:46 +0200

    first commit

---
+++ canti.js
@@ -0,0 +1,39 @@
+// canti.js
+
+const canti = [
+  {
+    titolo: "Chiamati per nome",
+    categoria: "Ingresso / Finale",
+    testo: "<b>RIT: Veniamo da te, chiamati per nome\nChe festa, Signore, tu cammini con noi\nCi parli di te, per noi spezzi il pane\nTi riconosciamo e il cuore arde, sei tu\nE noi tuo popolo, siamo qui</b>\n\nSiamo come terra ed argilla\ne la tua parola ci plasmerà\nbrace pronta per la scintilla\ne il tuo spirito soffierà. C'infiammerà <b>RIT.</b>\n\nSiamo come semi nel solco\nCome vigna che il suo frutto darà\nGrano del signore risorto\nLa tua messe che soffierà d'eternità <b>RIT.</b>\n"
+  },
+  {
+    titolo: "Frutto della nostra terra",
+    categoria: "Offertorio / Comunione",
+    testo: "Frutto della nostra terra, del lavoro di ogni uomo\npane della nostra vita, cibo della quotidianità.\nTu che lo prendevi un giorno,\nlo spezzavi per i tuoi,\noggi vieni in questo pane e ti doni per la vita mia.\n\n<b>RIT: E sarò pane, e sarò vino\nNella mia vita, nelle tue mani.\nTi accoglierò dentro di me\nFarò di me un’offerta viva,\nun sacrificio gradito a te.</b>\n\nFrutto della nostra terra, del lavoro di ogni uomo\nvino delle nostre vigne sulla mensa dei fratelli tuoi.\nTu che lo prendevi un giorno,\nlo bevevi con i tuoi.\nOggi vieni in questo vino\ne ti doni per la vita mia <b> RIT.</b>"
+  },
+  {
+    titolo: "Il canto dell'amore",
+    categoria: "Offertorio / Comunione",
+    testo: "Se dovrai attraversare il deserto,\nnon temere io sarò con te.\nSe dovrai camminare nel fuoco,\nla sua fiamma non ti brucerà\n\n<b>Seguirai la mia luce nella notte\nSentirai la mia forza nel cammino\nIo sono il tuo Dio, il Signore</b>\n\nSono io che ti ho fatto e plasmato,\nti ho chiamato per nome.\nIo da sempre ti ho conosciuto\ne ti ho datoil mio amore\n\n<b>Perché tu sei prezioso ai miei occhi\nVali più del più grande dei tesori\nIo sarò con te dovunque andrai</b>\n\nNon pensare alle cose di ieri, cose nuove fioriscono già\nAprirò nel deserto sentieri, darò acqua nell'aridità\n\n<b>Perché tu sei prezioso ai miei occhi\nVali più del più grande dei tesori\nIo sarò con te dovunque andrai</b>\n\nIo ti sarò accanto sarò con te!\nPer tutto il tuo viaggio\nsarò con te! (2v)"
+  },
+  {
+    titolo: "Re dei re",
+    categoria: "Ingresso / Finale",
+    testo: "Hai sollevato i nostri volti dalla polvere\nLe nostre colpe hai portato su di te\nSignore ti sei fatto uomo in tutto come noi,\nPer a-mo-re\n<b>RIT: Figlio dell'Altissimo, povero tra i poveri\nVieni a dimorare tra noi. Dio dell'impossibile,\nRe di tutti i secoli Vieni nella tua maestà\nRe dei re, i popoli ti acclamano\nI cieli ti proclamano Re dei re\nLuce degli uomini, regna col tuo amore\ntra noi, noi, noi, noi</b>\n\nCi hai riscattati dalla stretta delle tenebre\nPerché potessimo glorificare te\nHai riversato in noi la vita del tuo Spirito,\nPer a-mo-re <b>RIT.</b>\n\nTua è la glo – ria per se – mpre\nTua è la glo – ria per se – mpre\nGlo – ria, glo – ria Glo – ria, glo – ria <b>RIT.</b>"
+  },
+  {
+    titolo: "Soffierà",
+    categoria: "Ingresso / Finale",
+    testo: "Tu sei la prima stella del mattino,\ntu sei la nostra grande nostalgia,\ntu sei il cielo chiaro dopo la paura,\ndopo la paura di esserci perduti\ne tornerà la vita in questo mare.\n\n<b>RIT. Soffierà, soffierà il vento forte della vita\nSoffierà sulle vele e le gonfierà di te.\nSoffierà, soffierà il vento forte della vita,\nsoffierà sulle vele e le gonfierà di te.</b>\n\nTu sei l’unico volto della pace,\ntu sei speranza nelle nostre mani,\ntu sei il vento nuovo sulle nostre ali,\nsulle nostre ali, e soffierà la vita,\ne gonfierà le vele per questo mare. <b>RIT.</b>"
+  },
+  {
+    titolo: "Acqua siamo noi",
+    categoria: "Ingresso / Finale",
+    testo: "Acqua siamo noi dall’antica sorgente veniamo,\nfiumi siamo noi se i ruscelli si mettono insieme,\nmari siamo noi se i torrenti si danno la mano,\nvita nuova c’è se Gesù è in mezzo a noi.\n\n<b>RIT: E allora diamoci la mano\nE tutti insieme camminiamo\nEd un oceano di pace nascerà.\nE l’egoismo cancelliamo\nUn cuore limpido sentiamo\nE Dio che bagna del suo amor l’umanità.</b>\n\nSu nel cielo c’è Dio Padre che vive per l’uomo,\ncrea tutti noi e ci ama di amore infinito,\nfigli siamo noi e fratelli di Cristo Signore,\nvita nuova c’è quando Lui è in mezzo a noi. <b>RIT.</b>\n\nNuova umanità oggi nasce da chi crede in Lui,\nnuovi siamo noi se l’amore è la legge di vita,\nfigli siamo noi se non siamo divisi da niente,\nvita eterna c’è quando Lui è dentro a noi. <b>RIT.</b>"
+  },
+  {
+    titolo: "Servo per amore",
+    categoria: "Ingresso / Finale",
+    testo: "Una notte di sudore, sulla barca in mezzo al mare\ne mentre il cielo s’imbianca già,\ntu guardi le tue reti vuote.\nMa la voce che ti chiama, un altro mare ti mostrerà\ne sulle rive di ogni cuore, le tue reti getterai.\n\n<b>RIT: Offri la vita tua come Maria\nai piedi della croce e sarai,\nservo di ogni uomo, servo per amore\nsacerdote dell’umanità.</b>\n\nAvanzavi nel silenzio, fra le lacrime e speravi\nche il seme sparso davanti a te, cadesse sulla buonaterra. \nOra il cuore tuo è in festa, perché il grano biondeggia ormai,\n è maturato sotto il sole, puoi riporlo nei granai. <b>RIT.</b>"
+  }
+];
